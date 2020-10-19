3fb23fed72675b9aaf901bb9b4c01c42be4767c7
elmo0082 <ivomitcomet@gmail.com>
2020-10-19 15:07:30 +0200

    New loss functions and refined computation of the loss, also added subdivide algorithm to the possible loss functions.

--- rasenna/src/rasenna/criteria/CustomLossFunctions.py
+++ rasenna/src/rasenna/criteria/CustomLossFunctions.py
@@ -5,10 +5,76 @@ import time
 from inferno.extensions.criteria.set_similarity_measures import SorensenDiceLoss
 from rasenna.criteria.TopologicalLossFunction import TopologicalLossFunction
 from speedrun.log_anywhere import log_scalar
+from rasenna.criteria.Subdivision import SubdivisionLossFunction
 
-class TopologicalSDLoss(nn.Module):
+class TopologicalLoss(nn.Module):
     """
-    Computes a weighted loss scalar which uses a balance between two SorensenDice Losses, 
+    Computes a weighted loss scalar which uses a balance between Sorensen-Dice loss and topological loss, 
+    one for all 20 channels and one acting only on the first three channels to compute boundary probabilities.
+    `input_or_target.size(1) = num_channels`.
+    """
+    def __init__(self, g_factor, weight=None, channelwise=True, eps=1e-6):
+        """
+        Parameters
+        ----------
+        :param g_factor: float
+            This controls the weighting of Sorensen-Dice vs. topological loss
+        :param weight: torch.FloatTensor or torch.cuda.FloatTensor
+            Class weights: Applies only if `channelwise = True`.
+        :param channelwise: bool
+            Whether to apply the loss channelwise and sum the results (True)
+            or to apply it on all channels jointly (False).
+        """
+        super(TopologicalLoss, self).__init__()
+        self.register_buffer('weight', weight)
+        self.channelwise = channelwise
+        self.eps = eps
+        self.g_factor = g_factor
+
+        self.SDLoss = SorensenDiceLoss()
+        self.TopoLoss = TopologicalLossFunction.apply
+
+    def forward(self, input, target):
+        """
+        Parameters
+        ----------
+        :param input:      torch.FloatTensor or torch.cuda.FloatTensor
+        :param target:     torch.FloatTensor or torch.cuda.FloatTensor
+        Expected shape of the inputs: (batch_size, nb_channels, ...)
+
+        Calculates the weighted sum of Sorensen-Dice loss and topological loss.
+        """
+        loss = 0.0
+
+        if self.g_factor != 0.0:
+            boundary_map = torch.bitwise_or(target[0,0,:,:,:].bool(), target[0,1,:,:,:].bool())
+            boundary_map = torch.bitwise_or(boundary_map, target[0,2,:,:,:].bool())
+            boundary_prob = (1/3) * (input[0,0,:,:,:] + input[0,1,:,:,:] + input[0,2,:,:,:])
+
+            # We have to invert the values to be able to use persistent homology
+            boundary_prob = 1 - boundary_prob
+            boundary_map = 1 - boundary_map.float()
+
+            # we have to put a minus sign here, see https://www.jeremyjordan.me/semantic-segmentation/
+            sorensen_dice_loss = 1 - self.SDLoss(input, target)
+            topological_loss = self.TopoLoss(boundary_prob, boundary_map.float()).cuda()
+
+            # logging of the different losses in tensorboardX
+            log_scalar('training_loss/SorensenDice', sorensen_dice_loss)
+            log_scalar('training_loss/Topological', topological_loss)
+            
+            loss = sorensen_dice_loss + self.g_factor * topological_loss
+            print('Topological Loss:', topological_loss, 'Sorensen-Dice Loss:', sorensen_dice_loss, "Loss:", loss)
+        else:
+            loss = 1 - self.SDLoss(input, target)
+            log_scalar('training_loss/SorensenDice', loss)
+            print("Sorensen-Dice Loss:", loss)
+
+        return loss
+
+class TopologicalBCELoss(nn.Module):
+    """
+    Computes a weighted loss scalar which uses a balance between binary crossentropy loss and topological loss, 
     one for all 20 channels and one acting only on the first three channels to compute boundary probabilities.
     `input_or_target.size(1) = num_channels`.
     """
@@ -24,11 +90,67 @@ class TopologicalSDLoss(nn.Module):
             Whether to apply the loss channelwise and sum the results (True)
             or to apply it on all channels jointly (False).
         """
-        super(TopologicalLoss, self).__init__()
+        super(TopologicalBCELoss, self).__init__()
         self.register_buffer('weight', weight)
         self.channelwise = channelwise
         self.eps = eps
         self.g_factor = g_factor
+
+        self.BCELoss = nn.BCELoss(weight=weight)
+        self.TopoLoss = TopologicalLossFunction.apply
+
+    def forward(self, input, target):
+        """
+        Parameters
+        ----------
+        :param input:      torch.FloatTensor or torch.cuda.FloatTensor
+        :param target:     torch.FloatTensor or torch.cuda.FloatTensor
+        Expected shape of the inputs: (batch_size, nb_channels, ...)
+        """
+        loss = 0.0
+
+        boundary_map = torch.bitwise_or(target[0,0,:,:,:].bool(), target[0,1,:,:,:].bool())
+        boundary_map = torch.bitwise_or(boundary_map, target[0,2,:,:,:].bool())
+        boundary_prob = (1/3) * (input[0,0,:,:,:] + input[0,1,:,:,:] + input[0,2,:,:,:])
+
+        # We have to invert the values to be able to use persistent homology
+        boundary_prob = 1 - boundary_prob
+        boundary_map = 1 - boundary_map.float()
+
+        # we have to put a minus sign here, see https://www.jeremyjordan.me/semantic-segmentation/
+        binary_cross_entropy_loss = self.BCELoss(input.flatten(), target.flatten())
+        topological_loss = self.TopoLoss(boundary_prob, boundary_map.float()).cuda()
+
+        # logging of the different losses in tensorboardX
+        log_scalar('training_loss/BCE', binary_cross_entropy_loss)
+        log_scalar('training_loss/Topological', topological_loss)
+        
+        loss = binary_cross_entropy_loss + self.g_factor * topological_loss
+        print('Topological Loss:', topological_loss, 'BCE Loss:', binary_cross_entropy_loss, "Loss:", loss)
+
+        return loss
+
+class TopologicalLossLogged(nn.Module):
+    """
+    A loss function that only returns the value of the Sorensen-Dice loss but also calculates the topological loss for comparison.
+    `input_or_target.size(1) = num_channels`.
+    """
+    def __init__(self, weight=None, channelwise=True, eps=1e-6):
+        """
+        Parameters
+        ----------
+        :param g_factor: float
+            This controls the weighting of SorensenDice vs. other Sore Loss
+        :param weight: torch.FloatTensor or torch.cuda.FloatTensor
+            Class weights: Applies only if `channelwise = True`.
+        :param channelwise: bool
+            Whether to apply the loss channelwise and sum the results (True)
+            or to apply it on all channels jointly (False).
+        """
+        super(TopologicalLossLogged, self).__init__()
+        self.register_buffer('weight', weight)
+        self.channelwise = channelwise
+        self.eps = eps
 
         self.SDLoss = SorensenDiceLoss()
         self.TopoLoss = TopologicalLossFunction.apply
@@ -59,16 +181,15 @@ class TopologicalSDLoss(nn.Module):
         log_scalar('training_loss/SorensenDice', sorensen_dice_loss)
         log_scalar('training_loss/Topological', topological_loss)
         
-        loss = sorensen_dice_loss + self.g_factor * topological_loss
+        loss = sorensen_dice_loss
         print('Topological Loss:', topological_loss, 'Sorensen-Dice Loss:', sorensen_dice_loss, "Loss:", loss)
 
         return loss
 
-class TopologicalBCELoss(nn.Module):
+class SubdivisionLoss(nn.Module):
     """
-    Computes a weighted loss scalar which uses a balance between two SorensenDice Losses, 
-    one for all 20 channels and one acting only on the first three channels to compute boundary probabilities.
-    `input_or_target.size(1) = num_channels`.
+    A loss function that uses the subdivision method/functioin to calculate the topological loss of a voxel. 
+    The topological loss is then weighted against the Sorensen-Dice loss.
     """
     def __init__(self, g_factor, weight=None, channelwise=True, eps=1e-6):
         """
@@ -82,14 +203,14 @@ class TopologicalBCELoss(nn.Module):
             Whether to apply the loss channelwise and sum the results (True)
             or to apply it on all channels jointly (False).
         """
-        super(TopologicalLoss, self).__init__()
+        super(SubdivisionLoss, self).__init__()
         self.register_buffer('weight', weight)
         self.channelwise = channelwise
         self.eps = eps
         self.g_factor = g_factor
 
-        self.BCELoss = nn.BCELoss()
-        self.TopoLoss = TopologicalLossFunction.apply
+        self.SDLoss = SorensenDiceLoss()
+        self.SubdivisionLoss = SubdivisionLossFunction.apply
 
     def forward(self, input, target):
         """
@@ -110,15 +231,14 @@ class TopologicalBCELoss(nn.Module):
         boundary_map = 1 - boundary_map.float()
 
         # we have to put a minus sign here, see https://www.jeremyjordan.me/semantic-segmentation/
-        binary_cross_entropy_loss = self.BCELoss(input.flatten(), target.flatten())
-        topological_loss = self.TopoLoss(boundary_prob, boundary_map.float()).cuda()
+        sorensen_dice_loss = 1 - self.SDLoss(input, target)
+        topological_loss = self.SubdivisionLoss(boundary_prob, boundary_map.float()).cuda()
 
         # logging of the different losses in tensorboardX
-        log_scalar('training_loss/BCE', binary_cross_entropy_loss)
+        log_scalar('training_loss/SorensenDice', sorensen_dice_loss)
         log_scalar('training_loss/Topological', topological_loss)
         
-        loss = binary_cross_entropy_loss + self.g_factor * topological_loss
-        print('Topological Loss:', topological_loss, 'Sorensen-Dice Loss:', binary_cross_entropy_loss, "Loss:", loss)
+        loss = sorensen_dice_loss + self.g_factor * topological_loss
+        print('Topological Loss:', topological_loss, 'Sorensen-Dice Loss:', sorensen_dice_loss, "Loss:", loss)
 
         return loss
-
--- rasenna/src/rasenna/criteria/TopologicalLossFunction.py
+++ rasenna/src/rasenna/criteria/TopologicalLossFunction.py
@@ -4,26 +4,35 @@ import torch
 import time
 import math
 from torch.autograd import Function
-
-# fix imports 
-import imp
-imp.load_dynamic('PersistencePython', '/home_sdb/jgrieser_tmp/anaconda3/envs/segmFr/lib/PersistencePython.so')
+from .PersistenceUtils import compute_persistence_2DImg, compute_dgm_force
+from .Subdivision import subdivide, assemble
+from speedrun.log_anywhere import log_scalar, log_image
 
 class TopologicalLossFunction(Function):
     """
-    Topological loss function for 2.5 dimensions, i.e. 1d homology on 3d voxel slices in z-direction
+    This loss-function calculates the topological loss of a gray-scale image/input in 2.5 dimensions, 
+    i.e. 1d homology on 3d voxel slices in z-direction. 
+    It is based on the code of Xiaoling Hu's paper --Topology-Preserving Deep Image Segmentation--
+    You can find the respective code under https://github.com/HuXiaoling/TopoLoss.
     """
 
     @staticmethod
     def forward(ctx, input, target):
         """
-        Forward pass of topological loss
+        Forward pass of topological loss function used to calculate the topological loss between input and target.
+        The loss is computed separately for each slice in z-direction and then added up. 
+        Not only does the algorithm calculate the loss, but it does also calculate the critical points, 
+        i.e. the points that --probably-- need to be fixed to get the right number of holes and thus reduce the loss.
         """
         ctx.x_size = input.shape[1]
         ctx.y_size = input.shape[2]
 
         loss = 0.0
         dgm_list = []
+
+        # log images boundary prob, boundary map
+        ctx.pred_pic = input[3]
+        ctx.target_pic = target[3]
 
         for i in range(0, len(input)):
             input_dgms, input_birth_cp, input_death_cp = compute_persistence_2DImg(input.cpu().detach()[i], dimension=1)
@@ -45,10 +54,11 @@ class TopologicalLossFunction(Function):
     @staticmethod
     def backward(ctx, grad_output):
         """
-        Backward pass of topological loss
+        Backward pass of topological loss used to calculate the gradient of the topological loss function.
+        To calculate the gradient, we need the critical points and their position in the 2d image.
+        We again calculate each gradient separately for each slice and then merge them into one 3d voxel, which is then used for backpropagation.
         """ 
         dgm_list = ctx.dgm_list
-
         grad_list = []
 
         for list_slice in dgm_list:
@@ -81,112 +91,26 @@ class TopologicalLossFunction(Function):
 
             thus we get a [length, width]-matrix with gradients as entries
             """
-            topo_grad[:, 2] = topo_grad[:, 2] * 2 # clarify the role of the minus sign here!!!
+            topo_grad[:, 2] = topo_grad[:, 2] * -2 # TODO clarify the role of the minus sign here!!!
 
             gradients = np.zeros((ctx.x_size, ctx.y_size))
 
             for pos in topo_grad:
-                gradients[int(pos[0]), int(pos[1])] = pos[2]
-            
+                gradients[int(pos[1]), int(pos[0])] = pos[2]            
             grad_list.append(torch.from_numpy(gradients))
+        
+        grad = grad_list[3].cuda()
+        indices = torch.nonzero(grad, as_tuple=True)
+        pic = ctx.pred_pic.index_put(indices=indices, values=torch.tensor(0.0))
 
+        red_pic = pic + grad
+        green_pic = pic
+        blue_pic = pic
+        rgb_in_pic = [red_pic.float(), green_pic.float(), blue_pic.float()]
+
+        log_image('output/prediction_and_gradient', torch.stack(rgb_in_pic, dim=0))
+        log_image('output/target', torch.stack([ctx.target_pic], dim=0))
+        
+        # Stack all gradients along z-direction
         return torch.stack(grad_list, dim=0).cuda(), None
 
-def compute_persistence_2DImg(f, dimension):
-    """
-    compute persistence diagram in a 2D function (can be N-dim) and critical pts
-    only generate 1D homology dots and critical points
-    """
-    assert len(f.shape) == 2  # f has to be 2D function
-    dim = 2
-
-    # pad the function with a few pixels of maximum values
-    # this way one can compute the 1D topology as loops
-    # remember to transform back to the original coordinates when finished
-    # black = 1, white = 0
-    padwidth = 2
-    padvalue = min(f.min(), 0.0)
-    f_padded = np.pad(f, padwidth, 'constant', constant_values=padvalue)
-
-    # call persistence code to compute diagrams
-    # loads PersistencePython.so (compiled from C++); should be in current dir
-    from PersistencePython import cubePers
-    persistence_result = cubePers(np.reshape(
-        f_padded, f_padded.size).tolist(), list(f_padded.shape), 0.001)
-
-    # only take 1-dim topology, first column of persistence_result is dimension
-    persistence_result_filtered = np.array(list(filter(lambda x: x[0] == float(dimension), persistence_result)))
-
-    # persistence diagram (second and third columns are coordinates)
-    dgm = persistence_result_filtered[:, 1:3]
-
-    # critical points
-    birth_cp_list = persistence_result_filtered[:, 4:4 + dim]
-    death_cp_list = persistence_result_filtered[:, 4 + dim:]
-
-    # when mapping back, shift critical points back to the original coordinates
-    birth_cp_list = birth_cp_list - padwidth
-    death_cp_list = death_cp_list - padwidth
-
-    return dgm, birth_cp_list, death_cp_list
-
-def compute_dgm_force(lh_dgm, gt_dgm):
-    # get persistence list from both diagrams
-    lh_pers = lh_dgm[:, 1] - lh_dgm[:, 0]
-    gt_pers = gt_dgm[:, 1] - gt_dgm[:, 0]
-
-    # more lh dots than gt dots
-    assert lh_pers.size >= gt_pers.size
-
-    # check to ensure that all gt dots have persistence 1
-    tmp = gt_pers > 0.999
-    # assert tmp.sum() == gt_pers.size
-
-    gt_n_holes = gt_pers.size  # number of holes in gt
-
-    # get "perfect holes" - holes which do not need to be fixed, i.e., find top
-    # lh_n_holes_perfect indices
-    # check to ensure that at least one dot has persistence 1; it is the hole
-    # formed by the padded boundary
-    # if no hole is ~1 (ie >.999) then just take all holes with max values
-    tmp = lh_pers > 0.999  # old: assert tmp.sum() >= 1
-    if tmp.sum() >= 1:
-        # n_holes_to_fix = gt_n_holes - lh_n_holes_perfect
-        lh_n_holes_perfect = tmp.sum()
-        idx_holes_perfect = np.argpartition(lh_pers, -lh_n_holes_perfect)[
-                            -lh_n_holes_perfect:]
-    else:
-        idx_holes_perfect = np.where(lh_pers == lh_pers.max())[0]
-
-    # find top gt_n_holes indices
-    idx_holes_to_fix_or_perfect = np.argpartition(lh_pers, -gt_n_holes)[
-                                  -gt_n_holes:]
-
-    # the difference is holes to be fixed to perfect
-    idx_holes_to_fix = list(
-        set(idx_holes_to_fix_or_perfect) - set(idx_holes_perfect))
-
-    # remaining holes are all to be removed
-    idx_holes_to_remove = list(
-        set(range(lh_pers.size)) - set(idx_holes_to_fix_or_perfect))
-
-    # only select the ones whose persistence is large enough
-    # set a threshold to remove meaningless persistence dots
-    # TODO values below this are small dents so dont fix them; tune this value?
-    pers_thd = 0.03
-    idx_valid = np.where(lh_pers > pers_thd)[0]
-    idx_holes_to_remove = list(
-        set(idx_holes_to_remove).intersection(set(idx_valid)))
-
-    force_list = np.zeros(lh_dgm.shape)
-    # push each hole-to-fix to (0,1)
-    force_list[idx_holes_to_fix, 0] = 0 - lh_dgm[idx_holes_to_fix, 0]
-    force_list[idx_holes_to_fix, 1] = 1 - lh_dgm[idx_holes_to_fix, 1]
-
-    # push each hole-to-remove to (0,1)
-    force_list[idx_holes_to_remove, 0] = lh_pers[idx_holes_to_remove] / \
-                                         math.sqrt(2.0)
-    force_list[idx_holes_to_remove, 1] = -lh_pers[idx_holes_to_remove] / \
-                                         math.sqrt(2.0)
-
-    return force_list, idx_holes_to_fix, idx_holes_to_remove

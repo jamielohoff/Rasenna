3074ce85b8b7e81f24f01bdd705667e0f2971350
elmo0082 <ivomitcomet@gmail.com>
2021-03-01 18:11:55 +0100

    Added compilation of cPers to setup.py

--- rasenna/setup.py
+++ rasenna/setup.py
@@ -18,6 +18,13 @@ except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
+module = Extension('PersistencePython,
+                   sources = ['cPers/cPers/PersistencePython.cpp', 
+                   'cPers/cPers/Debugging.cpp', 'cPers/cPers/PersistenceIO.cpp',],
+                   include_dirs = ['cPers/blitz'],
+                   extra_compile_args=['-fPIC', '-O3', '-w', '-shared', '-std=c++11', '-I cPers/cPers/pybind11-stable/include `python3.7-config --cflags --ldflags --libs`'])
+
+
 
 if __name__ == "__main__":
     setup(use_pyscaffold=True,

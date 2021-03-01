406cb756c4989da34b9154debc9b74d3b0c7b3b4
elmo0082 <ivomitcomet@gmail.com>
2021-03-01 17:57:42 +0100

    Removed opencv from requirements

--- rasenna/setup.py
+++ rasenna/setup.py
@@ -31,6 +31,5 @@ if __name__ == "__main__":
                 "h5py",
                 "torch",
                 "matplotlib", 
-                "opencv",
                 "tensorboardx",
             ],)

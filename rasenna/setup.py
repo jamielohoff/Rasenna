f8ae7bd68aee88980714e0953679e4f2766edcbb
elmo0082 <ivomitcomet@gmail.com>
2021-03-01 17:54:16 +0100

    Modified setup.py

--- rasenna/setup.py
+++ rasenna/setup.py
@@ -33,5 +33,5 @@ if __name__ == "__main__":
                 "matplotlib", 
                 "opencv",
                 "tensorboardx",
-                "multiprocessing"
-            ])
+                "multiprocessing",
+            ],)
